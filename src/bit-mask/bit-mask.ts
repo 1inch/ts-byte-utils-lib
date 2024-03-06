a4f93c00d3059d3c27d155866dca1c9e6647518b
Vladimir Borovik <v.borovik@1inch.io>
2024-03-06 18:32:05 +0300

    more docs

--- src/bit-mask/bit-mask.ts
+++ src/bit-mask/bit-mask.ts
@@ -1,16 +1,17 @@
 import assert from 'assert'
 
+/**
+ * Class to define bit mask: new BitMask(16, 32) is for bits from [16, 32) => 0xffff0000
+ */
 export class BitMask {
     public readonly offset: bigint
 
     public readonly mask: bigint
 
     /**
-     *
-     *
      * @example
      * const mask1 = new BitMask(0, 16) // for bits from [0, 16) => 0xffff
-     * const mask2 = new BitMask(16, 32) // for bits from [0, 16) => 0xffff0000
+     * const mask2 = new BitMask(16, 32) // for bits from [16, 32) => 0xffff0000
      * const singleBit = new BitMask(10) // for 10th bit [10, 11)
      *
      * @param startBit bit position from the lowest bit, starts from zero, inclusive
