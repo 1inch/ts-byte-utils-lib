d208d64c0433fc831d2f6172636adefdafc16edd
Vladimir Borovik <v.borovik@1inch.io>
2024-06-06 15:40:05 +0300

    feat(constants): add `UINT_128_MAX`

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,5 @@ export const UINT_32_MAX = (1n << 32n) - 1n
 export const UINT_40_MAX = (1n << 40n) - 1n
 export const UINT_80_MAX = (1n << 80n) - 1n
 export const UINT_160_MAX = (1n << 160n) - 1n
+export const UINT_128_MAX = (1n << 128n) - 1n
 export const UINT_256_MAX = (1n << 256n) - 1n
